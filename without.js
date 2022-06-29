6a2dbf54b60e9059d7d8540708780940cd9c3338
Prathap <prathap.sims@gmail.com>
2022-06-29 07:12:42 +0000

    Implemented without.js

---
+++ without.js
@@ -0,0 +1,40 @@
+const assertArraysEqual = function(actual, expected) {
+
+  var result = eqArrays(actual, expected);
+
+  if (!result) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else  {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const eqArrays = function(arrayA, arrayB) {
+  if (arrayA.length !== arrayB.length) {
+     return false;
+  }
+
+  for (let i=0; i<= arrayA.length; i++) {
+    if (arrayA[i] !== arrayB[i]) {
+      return false;
+    }
+  }
+  return true ;
+};
+
+const without = function(source, itemsToRemove) {
+  let newArray = [];
+
+  for (let i = 0; i < source.length; i++) {
+    for(let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] != itemsToRemove[j]) {
+        newArray.push(source[i]);
+      }
+    }
+  }
+
+  console.log(newArray);
+};
+
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
