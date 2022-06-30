cedb740a56f5bb373357d32a5598ac8ae00bacde
Prathap <prathap.sims@gmail.com>
2022-06-30 06:04:00 +0000

    implemented countletters.js

---
+++ countLetters.js
@@ -0,0 +1,35 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else  {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const countLetters = function (letters) {
+  const countObj = {};
+  
+  letters = letters.replace('','');
+  
+  for (let letter of letters) {
+    if (!countObj[letter]) {
+      countObj[letter] = 1;
+    } else {
+      countObj[letter] ++;
+    }
+  }
+  return countObj;
+};
+
+// const result1 = countLetters('prathap');
+// console.log(result1);
+// assertEqual(result1["p"], 2);
+// assertEqual(result1["r"], 1);
+// assertEqual(result1["a"], 2);
+// assertEqual(result1["t"], 2);
+// assertEqual(result1["h"], 1);
+
+console.log(countLetters('prathap'));
+
+const result2 = countLetters("lighthouse in the house ");
+console.log(result2)
