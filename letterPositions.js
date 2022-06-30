a07ecc4e520ccfb0f3ab65d35d0f4921d15a55d0
Prathap <prathap.sims@gmail.com>
2022-06-30 06:50:48 +0000

    add letterposition.js

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+const assertArraysEqual = function(actual, expected) {
+
+  var result = eqArrays(actual, expected);
+
+  if (!result) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else  {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+const eqArrays = function(arrayA, arrayB) {
+  if (arrayA.length !== arrayB.length) {
+     return false;
+  }
+
+  for (let i=0; i<= arrayA.length; i++) {
+    if (arrayA[i] !== arrayB[i]) {
+      return false;
+    }
+  }
+  return true ;
+};
+const letterPositions = function(sentence) {
+  const results = {};
+  // logic to update results here
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (!results[sentence[i]]) {
+        results[sentence[i]] = [];
+      }
+      results[sentence[i]].push(i);
+    }
+  }
+
+  return results;
+};
+
+console.log(letterPositions("lighthouse in the house"));
+assertArraysEqual(letterPositions("hello").e, [1]);
