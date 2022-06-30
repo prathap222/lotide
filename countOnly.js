bc913005b3f7e7417decb0bfbcd15ab635154b1d
Prathap <prathap.sims@gmail.com>
2022-06-30 08:39:35 +0000

    added assert

--- countOnly.js
+++ countOnly.js
@@ -1,3 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else  {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
@@ -33,3 +41,8 @@ const firstNameObj = { "Jason": true, "Karima": true, "Fang": true, "Agouhanna":
 
 const result1 = countOnly(firstNames, firstNameObj);
 console.log(result1);
+
+assertEqual(result1["Jason"], 2);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
