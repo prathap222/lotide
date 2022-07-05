c42f4eaf863b5bb68f5a5dc14da07d50b586df0e
Prathap <prathap.sims@gmail.com>
2022-07-05 03:23:10 +0000

    refactored to modularize eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  } else  {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual !== expected) {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   } else  {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   }
+// };
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arrayA, arrayB) {
   if (arrayA.length !== arrayB.length) {
@@ -23,3 +24,5 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true);
+
+module.exports = eqArrays;
