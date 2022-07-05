e24cc616e8e07590a8112352b841491312d0de65
Prathap <prathap.sims@gmail.com>
2022-07-05 04:26:04 +0000

    refactored to modularize

--- eqArrays.js
+++ eqArrays.js
@@ -20,9 +20,9 @@ const eqArrays = function(arrayA, arrayB) {
   return true ;
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+// assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true);
 
 module.exports = eqArrays;
