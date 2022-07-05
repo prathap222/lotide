2dd63729fb8735c31b51682b2477929bd5c42dcf
Prathap <prathap.sims@gmail.com>
2022-07-05 00:24:48 +0000

    refactored to modularize

--- head.js
+++ head.js
@@ -1,10 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  } else  {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual !== expected) {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   } else  {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   }
+// };
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
   if (arr === []) {
