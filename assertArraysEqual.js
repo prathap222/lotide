17e85ee626f98223a694e59698e2f07a758511d4
Prathap <prathap.sims@gmail.com>
2022-07-05 04:58:36 +0000

    refactored to modularize assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -26,9 +26,9 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual([1, 2, 3], [1, 2, "3"]);
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3], [3, 2, 1]);
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+// assertArraysEqual([1, 2, 3], [1, 2, "3"]);
 
 module.exports = assertArraysEqual;
