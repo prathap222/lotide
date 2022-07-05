ee9f28849d85e90ceae42364c7364c2569e2b589
Prathap <prathap.sims@gmail.com>
2022-07-05 02:27:59 +0000

    refactored to modularize tail.js

--- tail.js
+++ tail.js
@@ -15,7 +15,7 @@ const tail = function(arr) {
   }
 };
 
-// // Test Case 1
+//  Test Case 1
 // const result = tail(["Hello", "Lighthouse", "Labs"]);
 // assertEqual(result.length, 2); // ensure we get back two elements
 // assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
