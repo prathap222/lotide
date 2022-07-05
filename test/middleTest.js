82e7a74fd8868e24eae28b8c04e48247c6c865b6
Prathap <prathap.sims@gmail.com>
2022-07-05 06:37:50 +0000

    added test for middle.js

---
+++ test/middleTest.js
@@ -0,0 +1,29 @@
+const middle = require('../middle');
+const assert = require('chai').assert;
+
+describe("#middle", () => {
+  it("returns empty-array if input if empty", () => {
+    let input = [];
+    let expected = [];
+    assert.equal(middle(input), expected);
+  });
+
+  it("returns empty-array if input-array has 2 elements", () => {
+    let input = [1, 2];
+    let expected = [];
+    assert.equal(middle(input), expected);
+  });
+
+  it("returns middle index for arrays with odd number of elements", () => {
+    let input = [1,2,3];
+    let expected = [2];
+    assert.equal(middle(input), expected);
+  });
+
+  it("returns 2 middle index for arrays with even number of elements", () => {
+    let input = [1, 2, 3, 4];
+    let expected = [2, 3];
+    assert.equal(middle(input), expected);
+  });
+
+});
