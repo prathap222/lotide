dda51149a685511144c561ab73b34640bfa40c1f
Prathap <prathap.sims@gmail.com>
2022-07-05 07:09:33 +0000

    refactored

--- tail.js
+++ tail.js
@@ -30,8 +30,8 @@ const tail = function(arr) {
 
 
 // Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
 
 module.exports = tail;
