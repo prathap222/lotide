45864b13bfa1177484d95ce85ec1156e02a5af5f
Prathap <prathap.sims@gmail.com>
2022-07-05 00:23:30 +0000

    refactored to modularize

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,18 @@
 const assertEqual = function(actual, expected) {
   if (actual !== expected) {
-    // console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return false;
   } else  {
-    // console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return true;
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+// console.log(assertEqual(1, 1));
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1)
+
+
+module.exports = assertEqual;
